32f65f4f7983d7a80d836b74c0a7025b0bc68d04
Joseph <josephedmed@hotmail.com>
2018-07-10 13:08:36 +0700

    Removed Create Admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,9 +5,9 @@ const Admin = require('../models/admin.js');
 const bcrypt = require('bcryptjs');
 
 // New Admin Route
-router.get('/', (req, res) => {
-  res.render('admin/new.ejs');
-})
+// router.get('/', (req, res) => {
+//   res.render('admin/new.ejs');
+// })
 
 // Create New Admin Route with Encrypted Password
 router.post('/', (req, res)=>{
